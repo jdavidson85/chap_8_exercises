cfddc84338c45729d84f4d25350123c9ba08e792
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-03-17 20:38:48 -0500

    Add files via upload

--- ch_8_exercises.py
+++ ch_8_exercises.py
@@ -11,32 +11,38 @@
 print("=" * 10, "Section 8.1 string operations", "=" * 10)
 # 1) Print all the characters from the name variable by accessing one character at a time
 name = "John Jacob Jingleheimer Schmidt"
-
+for letter in name:
+    print(letter)
 # 2) Use the index value to access and print the capital s in Schmidt from the variable name
-
+print(name[24])
 # 3) Use a negative index value to print the letters from the last name "Schmidt" in name
-
+print(name[-7:])
 
 # TODO 8.2 String slicing
 print("=" * 10, "Section 8.2 string slicing", "=" * 10)
 # 1) Use string slicing to assign the middle name "Jacob" from name to the variable middle, replace the ""
 # 2) Print middle
-middle = ""
+middle = name[5:10]
+print(middle)
 
 
 # TODO 8.3 Testing, Searching, and manipulating strings
 print("=" * 10, "Section 8.3 manipulating strings", "=" * 10)
 # 1) Test to see if the string "Jacob" is in name, print the result
-
+if "jacob" in name:
+    print("Jacob is in the name.")
+else:
+    print("Jacob is not in the name.")
 # 2) Test to see if the string "Michael" is in name, print the result
-
+print("Michael" in name)
 # 3) Test to see if name contains only digits, print the result
 #    Hint: Use the built-in string testing methods
-
+print("the name contains only digits:", name.isdigit())
 # 4) Test to see if name is lowercase, print the result
-
+print("The name is lowercase:", name.islower())
 # 5) Search for "J" in name, replace with "j" (lower case), print the result
 #    Note: You can assign this to a variable first, or just print
-
+print(name.replace("J", "j"))
 # 6) Split the string name into the variable name_list below (replace the ""), print the result
-name_list = ""
+name_list = name.split()
+print(name_list)
